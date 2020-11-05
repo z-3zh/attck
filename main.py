04077778c5d20bf9c01ccee7596d38d886a94b3e
mzh <z-3zh@hotmail.com>
2020-11-05 09:32:50 +0800

    1

--- main.py
+++ main.py
@@ -22,22 +22,25 @@ def get_techurl(url):
     techurls = []
     req = requests.get(url).text
     soup = BeautifulSoup(req, "lxml")
-    tables = soup.findAll('table')
-    tab = tables[0]
-    for tr in tab.findAll('tr'):
-        for td in tr.findAll('td'):
-            for h in td.findAll('a',href=True):
-                techurls.append(h['href'])
-    return list(set(techurls))
+    data = [[cell for cell in row.find_all('td')[1:2]]
+            for row in soup.find_all("tr", class_=True)]
+    for d in data:
+        techurls.append(d[0].findAll('a')[0]['href'])
+    return techurls
 
-def get_finalurls(url):
+
+def get_info(url):
     for u in get_techurl(url):
         print(get_techurl(u))
 
+
 if __name__ == '__main__':
     url = 'https://attack.mitre.org/tactics/enterprise/'
+    # u = 'https://attack.mitre.org/tactics/TA0002'
+    # get_techurl(u)
     for tu in get_taurl(url):
-        print(get_techurl(main_url + tu))
+        for s in get_techurl(main_url + tu):
+            print(s)
 
 
 
