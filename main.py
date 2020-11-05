d14f734e93c3108d8bb4ee7a4ef24351870a392f
mzh <z-3zh@hotmail.com>
2020-11-06 07:56:18 +0800

    finsh

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import requests
-import lxml
 from bs4 import BeautifulSoup
+import requests  # python3.6   pip install requests
+from pyquery import PyQuery as pq  # python3.6   pip install pyquery
+import csv
 
 main_url = "https://attack.mitre.org"
 finalurls = []
@@ -29,18 +30,45 @@ def get_techurl(url):
     return techurls
 
 
-def get_info(url):
-    for u in get_techurl(url):
-        print(get_techurl(u))
+def spider(url):
+    data_list = []
+    try:
+        response = requests.get(url)
+        doc = pq(response.text)
+        for item in doc(".card-body").find("div").items():
+            data_list.append(item.text().replace(u'\xa0', ''))
+    except Exception as e:
+        print("url: {}, error: {}".format(url, e))
+    return data_list
+
+
+# def get_info(url):
+#     req = requests.get(url).text
+#     soup = BeautifulSoup(req, "lxml")
+#     data = soup.findAll('div', class_='card-body')
+#     print(data.findAll('span', class_='h5 card-title'))
+
+def write_csv(result):
+    with open('result.csv', 'a') as csv_file:
+        writer = csv.writer(csv_file)
+        for f in result:
+            writer.writerow([f])
 
 
 if __name__ == '__main__':
     url = 'https://attack.mitre.org/tactics/enterprise/'
-    # u = 'https://attack.mitre.org/tactics/TA0002'
-    # get_techurl(u)
+    # u = 'https://attack.mitre.org/techniques/T1591/'
+    # write_csv(spider(u))
+
     for tu in get_taurl(url):
         for s in get_techurl(main_url + tu):
-            print(s)
+            write_csv(spider(main_url + s))
+
+    # url_list = [
+    #     "https://attack.mitre.org/techniques/T1591/"
+    # ]  # ["T1591", "T1592", "T1591"] 添加url即可  以,分隔
+    # for url in url_list:
+    #     print(spider(url))
 
 
 
